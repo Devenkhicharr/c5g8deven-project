bb047ca07a1e3e3fcac1e042c6c4d9521126de97
Devenkhicharr <99933280+Devenkhicharr@users.noreply.github.com>
2022-03-06 12:25:00 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,20 @@
+marks = int(input("type your result"))
+if  marks >0 and marks <30:
+    print("practice more to get good marks")
+elif marks<90 and marks ==100:    
+    print("nice work")
+elif marks <30 and marks>.50:
+     print("little more practice")
+else :
+     print("good score")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
